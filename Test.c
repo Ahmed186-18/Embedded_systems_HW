2a3e25e669ac9d5fcba40101118e37b87859eb97
Ahmed H Alkahlout <80820212+Ahmed186-18@users.noreply.github.com>
2024-12-28 23:04:17 +0200

    Update Test.c

--- Test.c
+++ Test.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+#define VERSION "1.0.0"
+
 int main() {
-    printf("Hello, World!\n");
+    char name[50];
+    printf("Program Version: %s\n", VERSION);
+    printf("Enter your name: ");
+    scanf("%s", name);
+    printf("Hello, %s! Welcome to the program.\n", name);
     return 0;
 }
